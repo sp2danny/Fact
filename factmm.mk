6cc656758ada034f941e08a7ce56ca7cd82c6cb3
Daniel Nyström <sp2danny@gmail.com>
2018-12-09 02:25:38 +0100

    update

---
+++ factmm.mk
@@ -0,0 +1,134 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Release
+ProjectName            :=factmm
+ConfigurationName      :=Release
+WorkspacePath          :=/home/daniel/project/Fact
+ProjectPath            :=/home/daniel/project/Fact
+IntermediateDirectory  :=./bin/Release
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=daniel
+Date                   :=09/12/18
+CodeLitePath           :=/home/daniel/.codelite
+LinkerName             :=/usr/bin/clang++
+SharedObjectLinkerName :=/usr/bin/clang++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(ProjectName).out
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="factmm.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch)/usr/include/gtk-2.0 $(IncludeSwitch)/usr/include/glib-2.0 $(IncludeSwitch)/usr/lib/x86_64-linux-gnu/glib-2.0/include $(IncludeSwitch)/usr/include/cairo $(IncludeSwitch)/usr/include/pango-1.0 $(IncludeSwitch)/usr/lib/x86_64-linux-gnu/gtk-2.0/include $(IncludeSwitch)/usr/include/gdk-pixbuf-2.0 $(IncludeSwitch)/usr/include/atk-1.0 $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := $(LibrarySwitch)gtk-x11-2.0 $(LibrarySwitch)gdk-x11-2.0 $(LibrarySwitch)pangocairo-1.0 $(LibrarySwitch)atk-1.0 $(LibrarySwitch)cairo $(LibrarySwitch)gdk_pixbuf-2.0 $(LibrarySwitch)gio-2.0 $(LibrarySwitch)pangoft2-1.0 $(LibrarySwitch)pango-1.0 $(LibrarySwitch)gobject-2.0 $(LibrarySwitch)glib-2.0 $(LibrarySwitch)fontconfig $(LibrarySwitch)freetype $(LibrarySwitch)boost_system $(LibrarySwitch)boost_filesystem $(LibrarySwitch)pthread $(LibrarySwitch)gmp $(LibrarySwitch)gmpxx 
+ArLibs                 :=  "gtk-x11-2.0" "gdk-x11-2.0" "pangocairo-1.0" "atk-1.0" "cairo" "gdk_pixbuf-2.0" "gio-2.0" "pangoft2-1.0" "pango-1.0" "gobject-2.0" "glib-2.0" "fontconfig" "freetype" "boost_system" "boost_filesystem" "pthread" "gmp" "gmpxx" 
+LibPath                := $(LibraryPathSwitch). $(LibraryPathSwitch)/usr/lib/x86_64-linux-gnu/ 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/clang++
+CC       := /usr/bin/clang
+CXXFLAGS := -std=c++17 -Wall -Wextra -Werror -pedantic -O2 -Wall $(Preprocessors)
+CFLAGS   :=  -O2 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/src_factmm.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_graph.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_mandel_hp.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d ./bin/Release || $(MakeDirCommand) ./bin/Release
+
+
+$(IntermediateDirectory)/.d:
+	@test -d ./bin/Release || $(MakeDirCommand) ./bin/Release
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/src_factmm.cpp$(ObjectSuffix): src/factmm.cpp $(IntermediateDirectory)/src_factmm.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/Fact/src/factmm.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_factmm.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_factmm.cpp$(DependSuffix): src/factmm.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_factmm.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_factmm.cpp$(DependSuffix) -MM src/factmm.cpp
+
+$(IntermediateDirectory)/src_factmm.cpp$(PreprocessSuffix): src/factmm.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_factmm.cpp$(PreprocessSuffix) src/factmm.cpp
+
+$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix): src/cmdline.cpp $(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/Fact/src/cmdline.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix): src/cmdline.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix) -MM src/cmdline.cpp
+
+$(IntermediateDirectory)/src_cmdline.cpp$(PreprocessSuffix): src/cmdline.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_cmdline.cpp$(PreprocessSuffix) src/cmdline.cpp
+
+$(IntermediateDirectory)/src_graph.cpp$(ObjectSuffix): src/graph.cpp $(IntermediateDirectory)/src_graph.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/Fact/src/graph.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_graph.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_graph.cpp$(DependSuffix): src/graph.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_graph.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_graph.cpp$(DependSuffix) -MM src/graph.cpp
+
+$(IntermediateDirectory)/src_graph.cpp$(PreprocessSuffix): src/graph.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_graph.cpp$(PreprocessSuffix) src/graph.cpp
+
+$(IntermediateDirectory)/src_mandel_hp.cpp$(ObjectSuffix): src/mandel_hp.cpp $(IntermediateDirectory)/src_mandel_hp.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/Fact/src/mandel_hp.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_mandel_hp.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_mandel_hp.cpp$(DependSuffix): src/mandel_hp.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_mandel_hp.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_mandel_hp.cpp$(DependSuffix) -MM src/mandel_hp.cpp
+
+$(IntermediateDirectory)/src_mandel_hp.cpp$(PreprocessSuffix): src/mandel_hp.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_mandel_hp.cpp$(PreprocessSuffix) src/mandel_hp.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./bin/Release/
+
+
---
+++ src/factmm.cpp
@@ -0,0 +1,304 @@
+
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <cstddef>
+#include <cassert>
+#include <cstdlib>
+#include <iomanip>
+#include <ios>
+
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
+
+#include "graph.h"
+#include "cmdline.h"
+#include "mandel_hp.h"
+
+cmdline cmd;
+
+Map m;
+Image img;
+
+unsigned long fuc = 1200;
+unsigned long update_cap = 100;
+unsigned long update_step = 10;
+float zoom_step = 2;
+float mod_base = 215.f;
+float mod_pow = 0.05f;
+unsigned long noupdatefor = 0;
+
+gboolean idle_func(gpointer data)
+{
+	if (m.map_all_done) return TRUE;
+
+	if (noupdatefor >= 100) return TRUE;
+
+	update_step += (update_step/10);
+	update_cap += update_step;
+
+	auto res = m.generate(update_cap, true);
+	if (res == Map::all_done)
+	{
+		std::cout << " --- all done ---   " << std::endl << std::flush;
+		m.map_all_done = true;
+		return TRUE;
+	}
+
+	if (res == Map::was_updated )
+	{
+		std::cout << "        working            " << "\r" << std::flush;
+		noupdatefor = 0;
+	}
+	if (res == Map::no_change)
+	{
+		noupdatefor += 1;
+		std::cout << "        unupdated " << noupdatefor << "\r" << std::flush;
+	}
+
+	float mod = mod_base / (float)std::pow(m.scale_x.get_d(), mod_pow);
+	img = m.makeimage(mod);
+
+	[[maybe_unused]]
+	GtkImage* image = (GtkImage*)data;
+
+	[[maybe_unused]]
+	GdkPixbuf* pbuf = gdk_pixbuf_new_from_data(
+		img.data(),
+		GDK_COLORSPACE_RGB,
+		FALSE,
+		8,
+		img.Width(),
+		img.Height(),
+		img.Width()*3,
+		nullptr,
+		nullptr
+	);
+
+	gtk_image_set_from_pixbuf(image, pbuf);
+
+	return TRUE;
+}
+
+gboolean delete_event(GtkWidget* widget, GdkEvent* event, gpointer data)
+{
+	(void)widget;
+	(void)event;
+	(void)data;
+
+	gtk_main_quit();
+	return TRUE;
+}
+
+void mk_img(GtkImage* image)
+{
+	noupdatefor = 0;
+	m.generate_init();
+	m.generate(update_cap, true);
+
+	float mod = mod_base / (float)pow(m.scale_x.get_d(), mod_pow);
+
+	img = m.makeimage(mod);
+
+	[[maybe_unused]]
+	GdkPixbuf* pbuf = gdk_pixbuf_new_from_data(
+		img.data(),
+		GDK_COLORSPACE_RGB,
+		FALSE,
+		8,
+		img.Width(),
+		img.Height(),
+		img.Width()*3,
+		nullptr,
+		nullptr
+	);
+
+	gtk_image_set_from_pixbuf(image, pbuf);
+}
+
+gboolean key_press(GtkWidget* widget, GdkEventKey* event, gpointer data)
+{
+	(void)widget;
+	(void)data;
+	//mp_exp_t expo;
+	switch (event->keyval)
+	{
+	case GDK_r:
+		update_cap = 100;
+		update_step = 10;
+		mk_img((GtkImage*)data);
+		break;	
+	case GDK_S:
+		img.Save("fact.bmp");
+		break;
+	case GDK_Q:
+		gtk_main_quit();
+		break;
+	case GDK_z:
+		zoom_step += 1.0l;
+		break;
+	case GDK_x:
+		zoom_step -= 1.0l;
+		break;
+	case GDK_space:
+		update_cap = 100;
+		update_step = 10;
+		m.scale_x /= zoom_step;
+		m.scale_y /= zoom_step;
+		mk_img((GtkImage*)data);
+		break;
+	case GDK_p:
+		std::cout << std::setprecision(50) ;
+		std::cout << "center-x     : " << m.center_x  << std::endl;
+		std::cout << "center-y     : " << m.center_y  << std::endl;
+		std::cout << "scale-x      : " << m.scale_x   << std::endl;
+		std::cout << "scale-y      : " << m.scale_y   << std::endl;
+		std::cout << "update cap   : " << update_cap  << std::endl;
+		std::cout << "update step  : " << update_step << std::endl;
+		std::cout << "trigger cap  : " << fuc         << std::endl;
+		std::cout << "zoom step    : " << zoom_step   << std::endl;
+		std::cout << "col-base     : " << mod_base    << std::endl;
+		std::cout << "col-pow      : " << mod_pow     << std::endl;
+		std::cout << "col-calc     : " << mod_base / (float)pow(m.scale_x.get_d(), mod_pow) << std::endl;
+		break;
+	case GDK_c:
+		fuc *= 1.1;
+		std::cout << "update cap   : " << update_cap  << std::endl;
+		std::cout << "update step  : " << update_step << std::endl;
+		std::cout << "trigger cap  : " << fuc         << std::endl;
+		break;
+	case GDK_v:
+		fuc *= 0.9;
+		std::cout << "update cap   : " << update_cap  << std::endl;
+		std::cout << "update step  : " << update_step << std::endl;
+		std::cout << "trigger cap  : " << fuc         << std::endl;
+		break;
+	//case GDK_m:
+	//	movie_maker(m, update_cap);
+	//	break;
+	case GDK_q:
+		mod_base *= 0.99f;
+		std::cout << "col-base     : " << mod_base    << std::endl;
+		std::cout << "col-pow      : " << mod_pow     << std::endl;
+		break;
+	case GDK_w:
+		mod_base *= 1.01f;
+		std::cout << "col-base     : " << mod_base    << std::endl;
+		std::cout << "col-pow      : " << mod_pow     << std::endl;
+		break;
+	case GDK_a:
+		mod_pow *= 0.99f;
+		std::cout << "col-base     : " << mod_base    << std::endl;
+		std::cout << "col-pow      : " << mod_pow     << std::endl;
+		break;
+	case GDK_s:
+		mod_pow *= 1.01f;
+		std::cout << "col-base     : " << mod_base    << std::endl;
+		std::cout << "col-pow      : " << mod_pow     << std::endl;
+		break;
+	}
+
+	return TRUE;
+}
+
+gboolean button_press(GtkWidget* widget, GdkEventButton* event, gpointer data)
+{
+	(void)widget;
+	(void)event;
+	(void)data;
+
+	if (event->type != GDK_BUTTON_PRESS)
+		return TRUE;
+
+    if (event->button == 3)
+    {
+       	m.scale_x *= zoom_step;
+		m.scale_y *= zoom_step;
+    }
+
+    if (event->button == 1)
+	{
+		Flt ldx = m.to_xpos(event->x);
+		Flt ldy = m.to_ypos(event->y);
+
+		m.center_x = ldx;
+		m.center_y = ldy;
+		m.scale_x /= zoom_step;
+		m.scale_y /= zoom_step;
+	}
+
+	update_cap = 100;
+	update_step = 10;
+
+	mk_img((GtkImage*)data);
+	return TRUE;
+}
+
+void gtk_app()
+{
+	GtkWidget* window;
+	GtkWidget* image;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(delete_event), nullptr);
+
+	gtk_container_set_border_width(GTK_CONTAINER(window), 8);
+
+	GtkWidget* eventbox = gtk_event_box_new();
+	gtk_container_add(GTK_CONTAINER(window), eventbox);
+
+	GdkPixbuf* pbuf = gdk_pixbuf_new_from_data(
+		img.data(),
+		GDK_COLORSPACE_RGB,
+		FALSE,
+		8,
+		img.Width(),
+		img.Height(),
+		img.Width()*3,
+		nullptr,
+		nullptr
+	);
+	image = gtk_image_new_from_pixbuf(pbuf);
+
+	g_signal_connect(G_OBJECT(window), "key-press-event", G_CALLBACK(key_press), image);
+	gtk_widget_set_events(eventbox, GDK_BUTTON1_MASK);
+	g_signal_connect(GTK_OBJECT(eventbox), "button-press-event", G_CALLBACK(button_press), image);
+
+	gtk_container_add(GTK_CONTAINER(eventbox), image);
+
+    gtk_widget_show(eventbox);
+	gtk_widget_show(image);
+	gtk_widget_show(window);
+	gtk_idle_add(&idle_func, image);
+	gtk_main();
+}
+
+int main(int argc, char* argv[])
+{
+	mpf_class mm(10.0, 512);
+	mm /= 3;
+	std::cout << std::setprecision(100) << mm << std::endl;
+	
+	cmd.init(argc, argv);
+
+	m.width    = std::stol(  cmd.get_parameter ("width",      "640"   ));
+	m.height   = std::stol(  cmd.get_parameter ("height",     "480"   ));
+	m.center_x =             cmd.get_parameter ("center-x",   "-0.75" ) ;
+	m.center_y =             cmd.get_parameter ("center-y",   "-0.0"  ) ;
+	m.scale_x  =             cmd.get_parameter ("scale-x",    "3.2"   ) ;
+	m.scale_y  = m.scale_x*3.0/4.0;
+	fuc        = std::stol(  cmd.get_parameter ("update-cap", "1200"  ));
+	zoom_step  = std::stol(  cmd.get_parameter ("zoom-step",  "2"     ));
+	mod_base   = std::stof(  cmd.get_parameter ("col-base",   "100"   ));
+	mod_pow    = std::stof(  cmd.get_parameter ("col-pow",    "0.04"  ));
+
+	m.generate_init();
+	float mod = mod_base / (float)pow(m.scale_x.get_d(), mod_pow);
+	img = m.makeimage(mod);
+
+	gtk_init(&argc, &argv);
+	gtk_app();
+}
+
