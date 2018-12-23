ba851c8424747eba7e646bfe756c72d5c578497e
Daniel Nyström <sp2danny@gmail.com>
2018-12-23 18:02:49 +0100

    update

---
+++ Server.mk
@@ -0,0 +1,118 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Release
+ProjectName            :=Server
+ConfigurationName      :=Release
+WorkspacePath          :=/home/daniel/project/Fact
+ProjectPath            :=/home/daniel/project/Fact
+IntermediateDirectory  :=./Release
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=daniel
+Date                   :=22/12/18
+CodeLitePath           :=/home/daniel/.codelite
+LinkerName             :=/usr/bin/clang++
+SharedObjectLinkerName :=/usr/bin/clang++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="Server.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch)/usr/include/gtk-2.0 $(IncludeSwitch)/usr/include/glib-2.0 $(IncludeSwitch)/usr/lib/x86_64-linux-gnu/glib-2.0/include $(IncludeSwitch)/usr/include/cairo $(IncludeSwitch)/usr/include/pango-1.0 $(IncludeSwitch)/usr/lib/x86_64-linux-gnu/gtk-2.0/include $(IncludeSwitch)/usr/include/gdk-pixbuf-2.0 $(IncludeSwitch)/usr/include/atk-1.0 $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := $(LibrarySwitch)gtk-x11-2.0 $(LibrarySwitch)gdk-x11-2.0 $(LibrarySwitch)pangocairo-1.0 $(LibrarySwitch)atk-1.0 $(LibrarySwitch)cairo $(LibrarySwitch)gdk_pixbuf-2.0 $(LibrarySwitch)gio-2.0 $(LibrarySwitch)pangoft2-1.0 $(LibrarySwitch)pango-1.0 $(LibrarySwitch)gobject-2.0 $(LibrarySwitch)glib-2.0 $(LibrarySwitch)fontconfig $(LibrarySwitch)freetype 
+ArLibs                 :=  "gtk-x11-2.0" "gdk-x11-2.0" "pangocairo-1.0" "atk-1.0" "cairo" "gdk_pixbuf-2.0" "gio-2.0" "pangoft2-1.0" "pango-1.0" "gobject-2.0" "glib-2.0" "fontconfig" "freetype" 
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/clang++
+CC       := /usr/bin/clang
+CXXFLAGS := -std=c++17 -Wall -Wextra -Werror -pedantic -O2 $(Preprocessors)
+CFLAGS   :=  -O2 $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/src_server.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d ./Release || $(MakeDirCommand) ./Release
+
+
+$(IntermediateDirectory)/.d:
+	@test -d ./Release || $(MakeDirCommand) ./Release
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/src_server.cpp$(ObjectSuffix): src/server.cpp $(IntermediateDirectory)/src_server.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/Fact/src/server.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_server.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_server.cpp$(DependSuffix): src/server.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_server.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_server.cpp$(DependSuffix) -MM src/server.cpp
+
+$(IntermediateDirectory)/src_server.cpp$(PreprocessSuffix): src/server.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_server.cpp$(PreprocessSuffix) src/server.cpp
+
+$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix): src/cmdline.cpp $(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/Fact/src/cmdline.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix): src/cmdline.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix) -MM src/cmdline.cpp
+
+$(IntermediateDirectory)/src_cmdline.cpp$(PreprocessSuffix): src/cmdline.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_cmdline.cpp$(PreprocessSuffix) src/cmdline.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Release/
+
+
---
+++ src/server.cpp
@@ -0,0 +1,117 @@
+
+
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <cstddef>
+#include <cassert>
+#include <cstdlib>
+#include <iomanip>
+#include <ios>
+
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
+
+#include "cmdline.h"
+
+cmdline cmd;
+
+gboolean idle_func(gpointer data)
+{
+	(void)data;
+	return TRUE;
+}
+
+gboolean delete_event(GtkWidget* widget, GdkEvent* event, gpointer data)
+{
+	(void)widget;
+	(void)event;
+	(void)data;
+
+	gtk_main_quit();
+	return TRUE;
+}
+
+
+
+gboolean key_press(GtkWidget* widget, GdkEventKey* event, gpointer data)
+{
+	(void)widget;
+	(void)data;
+	switch (event->keyval)
+	{
+	case GDK_Q:
+	case GDK_q:
+	case GDK_Escape:
+		gtk_main_quit();
+		break;
+	default:
+		break;
+	}
+
+	return TRUE;
+}
+
+gboolean button_press(GtkWidget* widget, GdkEventButton* event, gpointer data)
+{
+	(void)widget;
+	(void)event;
+	(void)data;
+
+	if (event->type != GDK_BUTTON_PRESS)
+		return TRUE;
+
+	return TRUE;
+}
+
+void gtk_app()
+{
+	GtkWidget* window;
+	//GtkWidget* image;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(delete_event), nullptr);
+
+	gtk_container_set_border_width(GTK_CONTAINER(window), 8);
+
+	GtkWidget* eventbox = gtk_event_box_new();
+	gtk_container_add(GTK_CONTAINER(window), eventbox);
+
+	/*
+	GdkPixbuf* pbuf = gdk_pixbuf_new_from_data(
+		img.data(),
+		GDK_COLORSPACE_RGB,
+		FALSE,
+		8,
+		img.Width(),
+		img.Height(),
+		img.Width()*3,
+		nullptr,
+		nullptr
+	);
+	image = gtk_image_new_from_pixbuf(pbuf);
+*/
+
+	g_signal_connect(G_OBJECT(window), "key-press-event", G_CALLBACK(key_press), nullptr);
+	gtk_widget_set_events(eventbox, GDK_BUTTON1_MASK);
+	g_signal_connect(GTK_OBJECT(eventbox), "button-press-event", G_CALLBACK(button_press), nullptr);
+
+	//gtk_container_add(GTK_CONTAINER(eventbox), image);
+
+    gtk_widget_show(eventbox);
+	//gtk_widget_show(image);
+	gtk_widget_show(window);
+	gtk_idle_add(&idle_func, nullptr);
+	gtk_main();
+}
+
+int main(int argc, char* argv[])
+{
+	cmd.init(argc, argv);
+
+	gtk_init(&argc, &argv);
+	gtk_app();
+}
+
