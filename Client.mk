c9b0ca4b3b1dae456b1e2eed77c95a25a6f18018
tooextdn <daniel.nystrom@external.atlascopco.com>
2019-01-09 15:26:00 +0100

    client 1st attempt

---
+++ Client.mk
@@ -0,0 +1,116 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Release
+ProjectName            :=Client
+ConfigurationName      :=Release
+WorkspacePath          := "/home/sp2danny/extra/Fact"
+ProjectPath            := "/home/sp2danny/extra/Fact"
+IntermediateDirectory  :=./bin/Release
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Daniel Nyström
+Date                   :=01/09/19
+CodeLitePath           :="/home/sp2danny/.codelite"
+LinkerName             :=/usr/bin/clang++ 
+SharedObjectLinkerName :=/usr/bin/clang++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=./$(ProjectName).out
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="Client.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/clang++ 
+CC       := /usr/bin/clang 
+CXXFLAGS :=  -O2 -Wall $(Preprocessors)
+CFLAGS   :=  -O2 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as 
+
+
+##
+## User defined environment variables
+##
+Objects0=$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_Client.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+$(IntermediateDirectory)/.d:
+	@test -d ./bin/Release || $(MakeDirCommand) ./bin/Release
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix): src/cmdline.cpp $(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/sp2danny/extra/Fact/src/cmdline.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix): src/cmdline.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_cmdline.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_cmdline.cpp$(DependSuffix) -MM "src/cmdline.cpp"
+
+$(IntermediateDirectory)/src_cmdline.cpp$(PreprocessSuffix): src/cmdline.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_cmdline.cpp$(PreprocessSuffix) "src/cmdline.cpp"
+
+$(IntermediateDirectory)/src_Client.cpp$(ObjectSuffix): src/Client.cpp $(IntermediateDirectory)/src_Client.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/sp2danny/extra/Fact/src/Client.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_Client.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/src_Client.cpp$(DependSuffix): src/Client.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_Client.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_Client.cpp$(DependSuffix) -MM "src/Client.cpp"
+
+$(IntermediateDirectory)/src_Client.cpp$(PreprocessSuffix): src/Client.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_Client.cpp$(PreprocessSuffix) "src/Client.cpp"
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) ./bin/Release/*$(ObjectSuffix)
+	$(RM) ./bin/Release/*$(DependSuffix)
+	$(RM) $(OutputFile)
+	$(RM) ".build-release/Client"
+
+
---
+++ src/Client.cpp
@@ -0,0 +1,130 @@
+
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <cstddef>
+#include <cassert>
+#include <cstdlib>
+#include <iomanip>
+#include <ios>
+#include <fstream>
+
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
+
+#include "connector.h"
+#include "cmdline.h"
+
+cmdline cmd;
+
+gboolean idle_func(gpointer data)
+{
+	(void)data;
+	return TRUE;
+}
+
+gboolean delete_event(GtkWidget* widget, GdkEvent* event, gpointer data)
+{
+	(void)widget;
+	(void)event;
+	(void)data;
+
+	gtk_main_quit();
+	return TRUE;
+}
+
+gboolean nil_cb(GtkWidget*, gpointer) { return TRUE; }
+
+GtkWidget* log3[3];
+std::vector<std::string> logs;
+
+void add_log(std::string s)
+{
+	logs.push_back(std::move(s));
+	int j, i, n = logs.size();
+	i = n-3;
+	if (i<0) i=0;
+	for (j=0;i<n;++i)
+	{
+		auto str = logs[i];
+		if ((i+1)==n)
+			str = ">>> " + str + " <<<";
+		gtk_label_set_text(GTK_LABEL(log3[j++]), str.c_str());
+	}
+}
+
+gboolean button_press(GtkWidget* widget, GdkEventButton* event, gpointer data)
+{
+	(void)widget;
+	(void)event;
+	(void)data;
+
+	if (event->type != GDK_BUTTON_PRESS)
+		return TRUE;
+
+	if (data == (void*)1)
+	{
+		add_log("Start");
+	}
+	if (data == (void*)2)
+	{
+		add_log("Stop");
+	}
+	if (data == (void*)3)
+	{
+		add_log("Log");
+	}
+	if (data == (void*)4)
+	{
+		gtk_main_quit();
+	}
+
+	return TRUE;
+}
+
+
+void gtk_app()
+{
+	GtkWidget* window;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(delete_event), nullptr);
+
+	gtk_container_set_border_width(GTK_CONTAINER(window), 8);
+
+	GtkWidget* buttons = gtk_vbutton_box_new();
+	gtk_widget_show(buttons);
+
+	GtkWidget* btn_start = gtk_button_new_with_label("Start");
+	GtkWidget* btn_stop  = gtk_button_new_with_label("Stop");
+	GtkWidget* btn_log   = gtk_button_new_with_label("Log");
+	GtkWidget* btn_exit  = gtk_button_new_with_label("Exit");
+
+	GtkWidget* btns[] = {btn_start, btn_stop, btn_log, btn_save, btn_load, btn_exit };
+
+	for (int i=0; i<4; ++i)
+	{
+		gtk_widget_show(btns[i]);
+		gtk_container_add(GTK_CONTAINER(buttons), btns[i]);
+		g_signal_connect(GTK_OBJECT(btns[i]), "button-press-event", G_CALLBACK(button_press), (void*)(intptr_t)(i+1));
+	}
+
+	gtk_container_add(GTK_CONTAINER(window), buttons);
+	gtk_widget_show(pane);
+
+	gtk_widget_show_all(window);
+	gtk_widget_show(window);
+	gtk_idle_add(&idle_func, nullptr);
+	gtk_main();
+}
+
+int main(int argc, char* argv[])
+{
+	cmd.init(argc, argv);
+
+	gtk_init(&argc, &argv);
+	gtk_app();
+}
+
